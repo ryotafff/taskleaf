19464a4b14e246481bd7ef1201f427a3cc5d5058
ryotafff <jnblog2019@gmail.com>
2020-09-24 09:11:04 +0000

    [finish]3-3-2-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,33 @@
 class TasksController < ApplicationController
+
   def index
-    @task = Task.new
+  	@tasks = Task.all
   end
 
   def show
   end
 
   def new
+  	@task = Task.new
+  end
+
+
+  def create
+  	task = Task.new(task_params)
+  	task.save!
+  	flash[:notice] = "タスク「#{task.name}」を登録しました。"
+  	redirect_to tasks_path
   end
 
   def edit
   end
+
+
+
+  private
+
+  def task_params
+  	params.require(:task).permit(:name, :description)
+  end
+
 end
