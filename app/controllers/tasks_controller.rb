0cec35658433aafb1b83838e929a6620505cac58
ryotafff <jnblog2019@gmail.com>
2020-10-01 07:38:11 +0000

    [finish]3-3-6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,6 +30,14 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
+  end
+
+
+
   private
 
   def task_params
