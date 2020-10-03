99e1b7604b971d5e6a5698b91704b419a0ef213c
ryotafff <jnblog2019@gmail.com>
2020-10-03 08:31:10 +0000

    [finish]4-5-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_074542) do
+ActiveRecord::Schema.define(version: 2020_10_03_082108) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_10_02_074542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tasks_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
