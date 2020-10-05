b09c37c13785f735d90a9504cbff46ce6601e005
ryotafff <jnblog2019@gmail.com>
2020-10-05 07:22:22 +0000

    [finish]4-5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
    resources :users
   end
